b935d49bb89f308d3ce9df70976ef34e7553a48d
jayara <jayarathambavita@gmail.com>
2019-07-26 00:38:59 +0530

    xxxb

--- src/app/Components/sign-up/sign-up.component.ts
+++ src/app/Components/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MustMatch } from '../_helpers/must-match.validator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,10 +10,12 @@ import { MustMatch } from '../_helpers/must-match.validator';
 })
 export class SignUpComponent implements OnInit {
   registerForm: FormGroup;
-  
+  closed=false;
+  show1=false;
+  goHome=false;
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,private router:Router) { }
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
@@ -35,8 +38,16 @@ export class SignUpComponent implements OnInit {
     if (this.registerForm.invalid) {
         return;
     }
-
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+    this.show1=true
+    if(this.goHome){
+    this.router.navigateByUrl("/")}
+}
+close(){
+  this.closed=true
+  this.show1=false
+  this.goHome=true
+  if(this.goHome){
+    this.router.navigateByUrl("/")}
 }
 
 }
