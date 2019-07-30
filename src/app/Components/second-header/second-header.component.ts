fa7afa8e8395b6fce93c7843a18c9756b5f0d564
ThushanLJ <thushanlakshitha97@gmail.com>
2019-07-30 22:27:45 +0530

    updated

--- src/app/Components/second-header/second-header.component.ts
+++ src/app/Components/second-header/second-header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-second-header',
@@ -10,14 +11,49 @@ import { Router } from '@angular/router';
 export class SecondHeaderComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
-              private router : Router) { }
+              private router : Router, private route : ActivatedRoute) { }
 
   searchForm : FormGroup;
+  private sub:any;
+  type:string;
+
+  public title1;
+  public title2;
+  public title3;
+  public title4;
+  
 
   ngOnInit() {
     this.searchForm = this.formBuilder.group({
       search:[null, Validators.required]
     });
+
+    this.sub=this.route.params.subscribe(params=>{
+      this.type=params['type'];
+    });
+
+
+    if(this.type == "knw"){
+      this.title1 = "style1"
+      this.title2 = "style2"
+      this.title3 = "style1"
+      this.title4 = "style1"
+    }else if(this.type == "ent"){
+      this.title1 = "style1"
+      this.title2 = "style1"
+      this.title3 = "style2"
+      this.title4 = "style1"
+    }else if(this.type == "inf"){
+      this.title1 = "style1"
+      this.title2 = "style1"
+      this.title3 = "style1"
+      this.title4 = "style2"
+    }else{
+      this.title1 = "style2"
+      this.title2 = "style1"
+      this.title3 = "style1"
+      this.title4 = "style1"
+    }
   }
 
   search(){
@@ -25,5 +61,37 @@ export class SecondHeaderComponent implements OnInit {
       this.router.navigate(['/search/'+this.searchForm.value.search]);
     }
   }
+
+
+  knw(){
+    this.title1 = "style1"
+      this.title2 = "style2"
+      this.title3 = "style1"
+      this.title4 = "style1"
+  }
+
+  ent(){
+    this.title1 = "style1"
+      this.title2 = "style1"
+      this.title3 = "style2"
+      this.title4 = "style1"
+  }
+
+  inf(){
+    this.title1 = "style1"
+      this.title2 = "style1"
+      this.title3 = "style1"
+      this.title4 = "style2"
+  }
+
+  home(){
+    this.title1 = "style2"
+      this.title2 = "style1"
+      this.title3 = "style1"
+      this.title4 = "style1"
+  }
+
+
+
 
 }
